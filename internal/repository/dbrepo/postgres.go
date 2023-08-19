1337726e7ff90170b5ce9a677d987f9e4636dba5
KingKord <93544537+KingKord@users.noreply.github.com>
2023-08-19 17:11:48 +0300

    Connecting A New Functions With Handlers

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -63,6 +63,7 @@ func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 	return nil
 }
 
+// SearchAvailabilityByDates returns true if availability exist for roomID, and false if no availability
 func (m *postgresDBRepo) SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
