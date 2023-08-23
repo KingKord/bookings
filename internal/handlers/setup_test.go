7178128b52275da3c960e48aa133aea7ce89db48
KingKord <93544537+KingKord@users.noreply.github.com>
2023-08-23 10:53:36 +0300

    updated tests and have done with email senders

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -41,6 +41,12 @@ func TestMain(m *testing.M) {
 	session.Cookie.Secure = app.InProduction
 
 	app.Session = session
+
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+	defer close(mailChan)
+
+	listenForMail()
 	tc, err := CreateTestTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
@@ -55,6 +61,14 @@ func TestMain(m *testing.M) {
 	render.NewRenderer(&app)
 
 	os.Exit(m.Run())
+}
+
+func listenForMail() {
+	go func() {
+		for {
+			_ = <-app.MailChan
+		}
+	}()
 }
 
 func getRoutes() http.Handler {
