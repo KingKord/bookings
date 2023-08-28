d432daa301ab0598f8a946fb7ac9f3405fd90553
KingKord <93544537+KingKord@users.noreply.github.com>
2023-08-28 09:48:29 +0300

    showing the reservation calendar II

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -113,3 +113,10 @@ func (m *testDBRepo) DeleteReservation(id int) error {
 func (m *testDBRepo) UpdateProcessedForReservation(id, processed int) error {
 	return nil
 }
+
+func (m testDBRepo) AllRooms() ([]models.Room, error) {
+
+	var rooms []models.Room
+
+	return rooms, nil
+}
