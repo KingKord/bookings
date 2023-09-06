104af1094de4a7a2d4f2be37ef85d1d87b9411f4
KingKord <93544537+KingKord@users.noreply.github.com>
2023-09-06 17:42:50 +0300

    migrations updated

---
+++ migrations/20230906132924_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+    ('Aleksandr','Gorbatykh','admin@admin.com','$2a$12$YPum98Xw2BTAsIbLyigis.YKqoWiGcGrLUhitJAukdqOFXnQFtk6K',3,'2023-08-25 00:00:00','2023-08-25 00:00:00');
